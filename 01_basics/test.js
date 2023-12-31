a99c88d074082b3755abc34dd37b74abd7267038
kratul9563 <149990041+kratul9563@users.noreply.github.com>
2023-12-31 05:41:19 +0000

    confige these operation

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,49 @@
-console.log("Kratul")
+// console.log("Kratul")
+
+
+
+/************************ Conversion **************************/
+
+let score="33abc"
+
+let validnumber=Number(score)     // it is using to convert string in number
+
+// console.log(typeof(validnumber));  //  this will show that it is type of  number 
+
+// console.log(validnumber);   // it will show that NaN it means it is not a number
+
+
+// 33 => 33
+// "33abc" =>NaN
+// true => 1
+// null=> 0
+// undefined => NaN
+
+
+let number= undefined
+let boolnumber=Boolean(number)
+
+// console.log(boolnumber)
+
+// 1 =>true
+// ""=>false
+// "Kratul"=>true
+// undefined =>false
+// true =>true
+
+let my = NaN
+let Change=String(my)
+
+// console.log(typeof Change)
+
+/******************************** Operation *************************/
+
+let value=6
+let negValue = -value
+
+// console.log(negValue)   // convert value in negative form
+
+let str1="Hello "
+let str2="Kratul"
+
+// console.log(str1+str2)     // we have join two string here 
