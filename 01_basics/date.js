8b55ea937c79c129862650c8cc5b8479a72f66dd
kratul9563 <149990041+kratul9563@users.noreply.github.com>
2024-01-01 05:32:10 +0000

    To find current Date and Time Zone

---
+++ 01_basics/date.js
@@ -0,0 +1,10 @@
+let myDate=new Date()
+
+console.log(myDate)
+console.log(myDate.toDateString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toTimeString())
+console.log(myDate.getUTCHours())
+console.log(typeof())
